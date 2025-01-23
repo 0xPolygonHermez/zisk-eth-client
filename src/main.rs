5df24434f3cf06f94f356a7cb5b18b198dd1236f
agnusmor <agnusmor@gmail.com>
2025-01-23 16:00:24 +0100

    Write public outputs

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ ziskos::entrypoint!(main);
 
 use rsp_client_executor::io::ClientExecutorInput;
 use rsp_client_executor::*;
-use ziskos::read_input;
+use ziskos::{read_input, set_output};
 
 fn main() {
     // Get the input slice from ziskos
     let input  = read_input();
-
+    
     let input = bincode::deserialize::<ClientExecutorInput>(&input).unwrap();
     let block_number = input.current_block.number;
 
@@ -22,6 +22,12 @@ fn main() {
 
     // Calculate block hash    
     let block_hash = header.hash_slow();
+
+    // Write block_hash value to the public output
+    for (index, chunk) in block_hash.to_vec().chunks(4).enumerate() {
+        let value =  u32::from_le_bytes(chunk.try_into().unwrap());
+        set_output(index, value);        
+    }
       
     // Print block number and calculated hash  
     println!("block number: {}, hash: {}\n", block_number, block_hash);
