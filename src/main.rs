7e6afd877936cd7d431f9813e18d0c3a39a71ad0
agnusmor <agnusmor@gmail.com>
2025-03-31 20:01:24 +0200

    Update rsp executor to last version

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,28 @@
 #![no_main]
 ziskos::entrypoint!(main);
 
-use rsp_client_executor::io::ClientExecutorInput;
-use rsp_client_executor::*;
+use rsp_client_executor::{
+    executor::EthClientExecutor,
+    io::EthClientExecutorInput,
+};
+use std::sync::Arc;
 use ziskos::{read_input, set_output};
 
 fn main() {
     // Get the input slice from ziskos
     let input  = read_input();
     
-    let input = bincode::deserialize::<ClientExecutorInput>(&input).unwrap();
+    let input = bincode::deserialize::<EthClientExecutorInput>(&input).unwrap();
     let block_number = input.current_block.number;
 
     println!("executing block {}", block_number);
 
     // Execute the block.
-    let executor = ClientExecutor;
-    let header = executor
-        .execute::<EthereumVariant>(input)
-        .expect("failed to execute client");
+    let executor = EthClientExecutor::eth(
+        Arc::new((&input.genesis).try_into().unwrap()),
+        input.custom_beneficiary,
+    );
+    let header = executor.execute(input).expect("failed to execute client");
 
     // Calculate block hash    
     let block_hash = header.hash_slow();
